1e5958ecf446d16c326f57722e12ce495fe7d236
audi <git@gitee.com:wang_neng_rui/gulimall.git>
2021-01-25 17:36:10 +0800

    第二周作业提交

---
+++ Week_02/HttpClient01.java
@@ -0,0 +1,34 @@
+package com.audi.nio;
+
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * @program: nio
+ * @author: wnr
+ * @Date 2021/1/25
+ */
+public class HttpClient01 {
+
+    public static void main(String[] args) throws Exception {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://localhost:8801");
+        CloseableHttpResponse response = null;
+        try {
+            response = httpClient.execute(httpGet);
+            if (response.getStatusLine().getStatusCode() == 200) {
+                String content = EntityUtils.toString(response.getEntity(), "UTF-8");
+                System.out.println(content);
+            }
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+            httpClient.close();
+        }
+    }
+}
---
+++ Week_02/各 GC收集器测试案例.md
@@ -0,0 +1,3 @@
+使用 GCLogAnalysis.java 自己演练一遍串行/并行/CMS/G1的案例
+
+![1611566165826](C:\Users\007\AppData\Roaming\Typora\typora-user-images\1611566165826.png)
